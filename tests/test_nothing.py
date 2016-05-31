1a7f8d7d94008c4a32ed28e36eb536652e5a2b89
Rajat Gupta <rajat.gupta712@gmail.com>
2016-05-31 22:33:05 +0530

    Added CI and tests

---
+++ tests/test_nothing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import unittest
+
+
+class TestNothing(unittest.TestCase):
+    def test_0010_test_nothing(self):
+        self.assertEqual(200, 200)
+
+
+def suite():
+    "Test suite"
+    test_suite = unittest.TestSuite()
+    test_suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase(TestNothing)
+    )
+    return test_suite
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())

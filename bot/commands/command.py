77bede56270c2217a2005e3776eb03be9295ffe5
Rajat Gupta <rajat.gupta712@gmail.com>
2016-06-03 07:31:45 +0530

    Added 2.7 support

--- bot/commands/command.py
+++ bot/commands/command.py
@@ -3,10 +3,11 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Command(metaclass=ABCMeta):
+class Command(object):
     """
     Command Object
     """
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def execute(self, command, channel):
